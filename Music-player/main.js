aff1f40ace43067f3eefe8c7c901e13b6d9c41c5
Sujatha Manoharan <Sujathamanoharan7@gmail.com>
2024-04-11 13:19:50 -0700

    song list has changed

--- Music-player/main.js
+++ Music-player/main.js
@@ -1,22 +1,70 @@
+
 let audioList = [
    
     {
-        img:'./images/image2.jpg',
-        audio:'./audios/audio2.mp3',
-        songName:'Second Song',
-        singerName:'SPB'
+        img:'./images/mookuthi-amman-movie-poster.webp',
+        audio:'./audios/Paarthene Amman Song.mp3',
+        songName:'Paarthene Amman Song',
+        singerName:'Hariharan'
     },
     {
-        img:'./images/image3.jpg',
-        audio:'./audios/audio3.mp3',
-        songName:'Third Song',
+        img:'./images/master-movie-poster.webp',
+        audio:'./audios/Azhagiya Puyale.mp3',
+        songName:'Master The blasterAzhagiya Puyale',
+        singerName:'GV Prakash'
+    },
+    {
+        img:'./images/soorarai-pottru-movie-poster.webp',
+        audio:'./audios/Kayilae Aagasam.mp3',
+        songName:'Kayile Aagasam',
+        singerName:'Pradeep'
+    },
+    {
+        img:'./images/soorarai-pottru-movie-poster.webp',
+        audio:'./audios/Maara Theme.mp3',
+        songName:'Maara Theme',
+        singerName:'Hariharan'
+    },
+    {
+        img:'./images/netrikann-movie-poster.webp',
+        audio:'./audios/Idhuvum Kadandhu Pogum The Healing Song.mp3',
+        songName:'Idhuvum Kadandhu Pogum',
+        singerName:'Pradeep'
+    }, {
+        img:'./images/teddy-movie-poster.webp',
+        audio:'./audios/Teddy Bear Rhyme.mp3',
+        songName:'Teddy Bear',
+        singerName:'GV Prakash'
+    },
+    {
+        img:'./images/jai-bhim-movie-poster.webp',
+        audio:'./audios/Thala Kodhum.mp3',
+        songName:'Thala Kodhum',
+        singerName:'Pradeep'
+    },
+    {
+        img:'./images/pushpa-the-rise-movie-poster.webp',
+        audio:'./audios/Srivalli.mp3',
+        songName:'Srivalli',
         singerName:'Sid Sriram'
     },
     {
-        img:'./images/image4.jpg',
-        audio:'./audios/audio4.mp3',
-        songName:'Fourth Song',
+        img:'./images/thunivu-movie-poster.webp',
+        audio:'./audios/Nenjame.mp3',
+        songName:'Nenjame',
         singerName:'Hariharan'
+    },
+    {
+        img:'./images/ponniyin-selvan-part-1-movie-poster.webp',
+        audio:'./audios/Ponni Nadhi.mp3',
+        songName:'Ponni Nadhi',
+        singerName:'The Legend'
+    },
+    {
+        img:'./images/varisu-movie-poster.webp',
+        audio:'./audios/Thee Thalapathy.mp3',
+        songName:'Thee Thalapathy',
+        singerName:'Anirudh'
     }
 ]
 const myImage = document.querySelector('img');
@@ -38,6 +86,7 @@ function playAudio(){
     myPlayIcon.classList.replace('fa-play','fa-pause');
 
 
+
 }
 function pauseAudio() {
     myAudio.pause();
@@ -48,15 +97,30 @@ function pauseAudio() {
 //events
 
 myPlayIcon.addEventListener("click",function(){
+    console.log(audioPlaying);
     if(audioPlaying){
         pauseAudio()
+        // myAudio.loop = false;
+    
     } else {
         playAudio();
+        // myAudio.loop = true;
+        // songsList(audioList[songIndex]);
+        // songIndex++;
+        // myAudio.play()
+
     }
 
 })
+myPlayIcon.addEventListener('ended', function () {
+    i = ++i < audioList.length ? i : 0;
+    console.log(i)
+    myAudio.src = audioList.audio[i];
+    playAudio();
+}, true);
 let songIndex = 0;
 function songsList(audioList){
+    console.log(audioList);
     myImage.src = audioList.img;
     myAudio.src = audioList.audio;
     myH2.textContent = audioList.songName;
@@ -84,7 +148,7 @@ backwardIcon.addEventListener("click", function(){
 })
 
 shuffleIcon.addEventListener("click", function() {
-    let randomNumberIndex = Math.floor(Math.random() * 3)
+    let randomNumberIndex = Math.floor(Math.random() * 12)
     songsList(audioList[randomNumberIndex]);
     playAudio();
 })
@@ -104,21 +168,22 @@ const myCurrentTime = document.querySelector(".currenttime")
 const innerProgress = document.querySelector(".innerprogress")
 
 myAudio.addEventListener("timeupdate", function(event){
-
-    let currentTimeFromEvent = event.target.currentTime;
+    let currentTime = event.target.currentTime;
     let duration = event.target.duration;
 
-    let audioPlayedDuration = currentTimeFromEvent/duration * 100
+    let audioPlayedDuration = currentTime/duration * 100
     innerProgress.style.width = `${audioPlayedDuration}%`
 
     let totalTimeInMins = Math.floor(duration/60);
     let totalTimeInSecs = Math.floor(duration % 60);
 
     myTotalTime.textContent = `${totalTimeInMins}:${totalTimeInSecs}`
-    let currentTimeInMins = Math.floor(currentTimeFromEvent/60);
-    let currentTimeInSecs = Math.floor(currentTimeFromEvent % 60);
 
-    mycurrentTime.textContent = `${currentTimeInMins}:${currentTimeInSecs}`
+
+    let currentTimeInMins = Math.floor(currentTime/60);
+    let currentTimeInSecs = Math.floor(currentTime % 60);
+
+    myCurrentTime.textContent = `${currentTimeInMins}:${currentTimeInSecs}`
 
 })
 
